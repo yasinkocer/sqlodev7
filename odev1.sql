1e9ea57a74e498919c744bcb67ae289d8743600a
Yasin Koçer <yasinreiz45@gmail.com>
2022-07-30 18:42:36 +0300

    first commit

---
+++ odev1.sql
@@ -0,0 +1,4 @@
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+
+SELECT rating FROM film
+GROUP BY rating;
---
+++ odev4.sql
@@ -0,0 +1,4 @@
+-- city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(country_id) FROM city
+GROUP BY country_id 
+ORDER BY COUNT(country_id) DESC;
